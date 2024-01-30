a8276f7e1358d86456bf7106ee8280f2e177ec96
gabriellerose <gabrielle@hitori-inc.com>
2024-01-30 16:34:25 +1100

    initial commit

---
+++ app/Main.js
@@ -0,0 +1,94 @@
+import React, { useState, useReducer, useEffect } from "react"
+import ReactDOM from "react-dom/client"
+import { useImmerReducer } from "use-immer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+import EditPost from "./components/EditPost"
+
+Axios.defaults.baseURL = "http://localhost:8080"
+
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
+
+// My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
+import Profile from "./components/Profile"
+
+function Main() {
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: [],
+    user: {
+      token: localStorage.getItem("complexappToken"),
+      username: localStorage.getItem("complexappUsername"),
+      avatar: localStorage.getItem("complexappAvatar")
+    }
+  }
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "login":
+        draft.loggedIn = true
+        draft.user = action.data
+        return
+      case "logout":
+        draft.loggedIn = false
+        return
+      case "flashMessage":
+        draft.flashMessages.push(action.value)
+        return
+    }
+  }
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexappToken", state.user.token)
+      localStorage.setItem("complexappUsername", state.user.username)
+      localStorage.setItem("complexappAvatar", state.user.avatar)
+    } else {
+      localStorage.removeItem("complexappToken")
+      localStorage.removeItem("complexappUsernam")
+      localStorage.removeItem("complexappAvatar")
+    }
+  }, [state.loggedIn])
+
+  //dispatch({ type: "login" })
+  //dispatch({ type: "logout" })
+  //dispatch({ type: "flashMessage", value: "Congrats, you made a post." })
+
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Routes>
+            <Route path="/profile/:username/*" element={<Profile />} />
+            <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
+            <Route path="/post/:id" element={<ViewSinglePost />} />
+            <Route path="/post/:id/edit" element={<EditPost />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/terms" element={<Terms />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<Main />)
+
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ connection-details.js
@@ -0,0 +1,4 @@
+// Mongo DB settings
+// username: gabrielrose
+// password: doobie
+// connectionstring: mongodb+srv://gabrielrose:doobie@cluster0.l0sdwpo.mongodb.net/ReactCourse?retryWrites=true&w=majority
